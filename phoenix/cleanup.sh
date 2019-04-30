dcd6d3561b247f2e39007295f2569f85097c7063
Eric Young <codefist0@gmail.com>
2019-04-30 10:19:38 -0500

    Make the phoenix script into single file

---
+++ phoenix/cleanup.sh
@@ -0,0 +1,1 @@
+rm -rf docker-compose.yml Dockerfile _build assets config deps lib priv test mix.exs mix.lock README.md .gitignore .formatter.exs
--- phoenix/dockstrap.sh
+++ phoenix/dockstrap.sh
@@ -1,5 +1,61 @@
 #!/usr/bin/env bash
 
+DOCKERFILE=$(cat <<HEREDOC
+FROM elixir
+
+# Install hex
+RUN mix local.hex --force
+
+# Install rebar
+RUN mix local.rebar --force
+
+# Install the Phoenix framework
+RUN mix archive.install hex phx_new 1.4.3 --force
+
+# Prep node to install via apt
+RUN curl -sL https://deb.nodesource.com/setup_12.x  | bash -
+
+RUN apt-get update
+RUN DEBIAN_FRONTEND=noninteractive apt-get -y install nodejs inotify-tools gcc g++ make
+
+WORKDIR /app
+HEREDOC
+)
+
+DOCKER_COMPOSE=$(cat <<HEREDOC
+version: "3"
+
+volumes:
+  pg-data:
+
+services:
+  app:
+    build:
+      context: .
+    ports:
+      - "4000:4000"
+    volumes:
+      - .:/app
+    depends_on:
+      - postgres
+    command:
+      mix phx.server
+
+  postgres:
+    image: postgres
+    environment:
+      POSTGRES_USER: postgres
+      POSTGRES_PASSWORD: postgres
+      PGDATA: /var/lib/postgresql/data
+    volumes:
+      - pg-data:/var/lib/postgresql/data
+HEREDOC
+)
+
+# write files
+echo "$DOCKERFILE" > Dockerfile
+echo "$DOCKER_COMPOSE" > docker-compose.yml
+
 echo Enter the app name:
 echo Phoenix uses a convention of
 echo lower-case letters with underscores
