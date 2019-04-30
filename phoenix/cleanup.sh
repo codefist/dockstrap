cad260c107e1b89872c3859016810908b56c31f7
Eric Young <codefist0@gmail.com>
2019-04-30 13:14:34 -0500

    Add priv dir to cleanup

--- phoenix/cleanup.sh
+++ phoenix/cleanup.sh
@@ -1,1 +1,1 @@
-rm -rf docker-compose.yml Dockerfile _build assets config deps lib priv test mix.exs mix.lock README.md .gitignore .formatter.exs
+rm -rf priv docker-compose.yml Dockerfile _build assets config deps lib priv test mix.exs mix.lock README.md .gitignore .formatter.exs
